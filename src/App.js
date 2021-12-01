6beba36a437f5e09a90a2ad4ddd34e7b220f7178
S-Bartlomiej <bstypula.github@gmail.com>
2021-12-01 10:38:23 +0100

    global styles zrobione i podlaczone

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar/index';
 import {BrowserRouter as Router} from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <>
     <Router>
-    <Navbar />
+            <GlobalStyle />
+             <Navbar />
+
     </Router>
     </>
   );
