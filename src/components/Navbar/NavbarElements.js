47676a01811628822ff7d1da9d42c74dbdd38915
S-Bartlomiej <bstypula.github@gmail.com>
2021-12-01 10:01:09 +0100

    Navbar zrobiony, teraz globalne style i hero section

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 import {NavLink as Link} from 'react-router-dom';
-import  LogoSushi  from '../../Images/logo-sushi.png';
+
 
 export const Nav = styled.nav`
     background: red;
-    height: 80px;
+    min-height: 100px;
     display: flex;
     justify-content: center;
     font-weight: 700;
@@ -18,27 +18,11 @@ export const NavLink = styled(Link)`
     text-decoration: none;
     cursor: pointer;
 
-    @media screen and (max-width: 600px) 
+    @media(max-width: 320x) 
     {
         position: absolute;
         top: 10px;
         left: 25px;
-    }
-
-`;
-
-export const NavIcon = styled.div`
-
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    cursor: pointer;
-    color: #fff;
-
-    p {
-        transform: translate(-175%, 100%);
-        font-weight: bold;
     }
 
 `;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import {Nav, NavLink, NavIcon, Bars} from './NavbarElements'
-import  LogozSushi  from '../../Images/logo-sushi.png';
+import {Nav, NavLink} from './NavbarElements'
 
 function Navbar() {
     return (
         <>
             <Nav>
                 <NavLink to='/'>Sushi</NavLink>
-                
-                <NavIcon>
-                    <p>Menu</p>
-                    <img src={LogozSushi} alt=""/>
-                </NavIcon>
-
             </Nav>
         </>
     );
