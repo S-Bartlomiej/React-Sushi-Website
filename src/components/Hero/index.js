b7be5052270bdbd460211c5ad5261c4e793b3b0f
S-Bartlomiej <bstypula.github@gmail.com>
2021-12-01 11:10:05 +0100

    pieknie dzialajace hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from '../Navbar'
-import { HeroContainer } from './HeroElements'
+import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroElements'
 
 function Hero() {
     return (
         <HeroContainer>
                 <Navbar />
+                    <HeroContent>
+                            <HeroItems>
+                                <HeroH1>Shrimply the best</HeroH1>
+                                <HeroP>Let the good times roll</HeroP>
+                                <HeroBtn>Zamów</HeroBtn>
+                            </HeroItems>
+                    </HeroContent>
         </HeroContainer>
     )
 }
