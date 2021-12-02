e87ac3f3396882779279c2fbc594170ccce80ca3
S-Bartlomiej <bstypula.github@gmail.com>
2021-12-02 07:44:44 +0100

    all sushi data provided

---
+++ src/components/Products/data.js
@@ -0,0 +1,36 @@
+export const productData = [
+    
+    {
+        img: product1,
+        alt: "Sushi",
+        name: "Sushi-Maki",
+        desc: 'Nori roll, rice, tuna, shrimp',
+        price: '$29.99 / 8pcs',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: "Sushi1",
+        name: "Sushi-Nigiri",
+        desc: 'Rice, wasabi, shrimp',
+        price: '$32,99 / 8pcs',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: "Sushi2",
+        name: "Sushi-Sashimi",
+        desc: 'Fish slices, vegetables',
+        price: '$24,99 / 8pcs',
+        button: 'Add to Cart'
+    },
+    {
+        img: product4,
+        alt: "Sushi3",
+        name: "Sushi-Uramaki",
+        desc: 'Rice, nori roll, cucumber, avocado, surimi',
+        price: '$51,99 / 8pcs',
+        button: 'Add to Cart'
+    }
+
+]
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Products() {
+    return (
+        <ProductsContainer>
+            <ProductsHeading>heading</ProductsHeading>
+            <ProductsWrapper>
+                
+            </ProductsWrapper>
+        </ProductsContainer>
+         
+        
+    )
+}
+
+export default Products
